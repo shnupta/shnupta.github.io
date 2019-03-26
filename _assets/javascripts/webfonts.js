a435ffb8725f336a5a47a15104709313672034d5
Casey Williams <williamshoops96@gmail.com>
2019-03-26 15:14:54 +0000

    change fonts attempt

--- _assets/javascripts/webfonts.js
+++ _assets/javascripts/webfonts.js
@@ -2,6 +2,8 @@
 
 WebFont.load({
   google: {
-    families: ['Cormorant Garamond:700', 'Lato:300,400,700']
+    families: ['Cormorant Garamond:700', 'Lato:300,400,700',
+	'Source Sans Pro:300,400,700'
+    ]
   }
 });
