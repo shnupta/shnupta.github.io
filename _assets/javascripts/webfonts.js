e1f649e7a3d7c95698c5907a2d9f183d1db382a1
Casey Williams <williamshoops96@gmail.com>
2019-03-26 15:30:34 +0000

    found fonts i like

--- _assets/javascripts/webfonts.js
+++ _assets/javascripts/webfonts.js
@@ -3,7 +3,7 @@
 WebFont.load({
   google: {
     families: ['Cormorant Garamond:700', 'Lato:300,400,700',
-    	'Quicksand:300,400,700', 'Slabo 27px:400,700'
+    	'Quicksand:300,400,700', 'Source Serif Pro:400,700'
     ]
   }
 });
